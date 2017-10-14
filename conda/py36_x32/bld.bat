59588a89c9c3eb4abf878fdbae553f26cdfdbab8
simonflueckiger <simon@flueckiger.ch>
2017-10-15 00:06:46 +0200

    support for different environments

---
+++ conda/py36_x32/bld.bat
@@ -0,0 +1,6 @@
+rem curl -fsS -L -o tesserocr-2.2.2-cp36-cp36m-win_amd64.whl https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/tesserocr-v2.2.2/tesserocr-2.2.2-cp36-cp36m-win_amd64.whl
+curl -fsS -L -o tesserocr-2.2.2-cp36-cp36m-win32.whl https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/tesserocr-v2.2.2/tesserocr-2.2.2-cp36-cp36m-win32.whl
+rem curl -fsS -L -o tesserocr-2.2.2-cp35-cp35m-win_amd64.whl https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/tesserocr-v2.2.2/tesserocr-2.2.2-cp35-cp35m-win_amd64.whl
+rem curl -fsS -L -o tesserocr-2.2.2-cp35-cp35m-win32.whl https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/tesserocr-v2.2.2/tesserocr-2.2.2-cp35-cp35m-win32.whl
+
+pip install tesserocr-2.2.2-cp36-cp36m-win32.whl
--- conda/py36_x64/bld.bat
+++ conda/py36_x64/bld.bat
@@ -1,2 +1,3 @@
 curl -fsS -L -o tesserocr-2.2.2-cp36-cp36m-win_amd64.whl https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/tesserocr-v2.2.2/tesserocr-2.2.2-cp36-cp36m-win_amd64.whl
+
 pip install tesserocr-2.2.2-cp36-cp36m-win_amd64.whl
