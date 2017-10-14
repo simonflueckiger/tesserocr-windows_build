bb410a5ca93f97ac96f10f5c14d0fe249bb7f6bf
simonflueckiger <simon@flueckiger.ch>
2017-10-15 00:41:46 +0200

    added Python 3.5 build

---
+++ conda/py35_x32/bld.bat
@@ -0,0 +1,3 @@
+curl -fsS -L -o tesserocr-2.2.2-cp35-cp35m-win32.whl https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/tesserocr-v2.2.2/tesserocr-2.2.2-cp35-cp35m-win32.whl
+
+pip install tesserocr-2.2.2-cp35-cp35m-win32.whl
---
+++ conda/py35_x64/bld.bat
@@ -0,0 +1,3 @@
+curl -fsS -L -o tesserocr-2.2.2-cp35-cp35m-win_amd64.whl https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/tesserocr-v2.2.2/tesserocr-2.2.2-cp35-cp35m-win_amd64.whl
+
+pip install tesserocr-2.2.2-cp35-cp35m-win_amd64.whl
