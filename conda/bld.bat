b1e73fbad00afe2537538a4b9d7cd0bcc7e3fd62
simonflueckiger <simon@flueckiger.ch>
2017-10-14 23:23:33 +0200

    bugfix in url

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,2 +1,2 @@
-curl -fsS -L -o tesserocr-2.2.2-cp36-cp36m-win_amd64.whl tesserocr-2.2.2-cp36-cp36m-win_amd64.whl
+curl -fsS -L -o tesserocr-2.2.2-cp36-cp36m-win_amd64.whl https://github.com/simonflueckiger/tesserocr-windows_build/releases/download/tesserocr-v2.2.2/tesserocr-2.2.2-cp36-cp36m-win_amd64.whl
 pip install tesserocr-2.2.2-cp36-cp36m-win_amd64.whl
