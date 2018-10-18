9d3aeee3f71ddbaec0e8f0d4ac23402080e607d6
Simon Flückiger <simon@flueckiger.ch>
2018-10-18 11:31:31 -0700

    added python 3.7 to build

--- res/build_32.bat
+++ res/build_32.bat
@@ -11,8 +11,14 @@ rem set PATH=C:\Miniconda35;C:\Miniconda35\Scripts;%PATH%
 rem conda install Cython PyYAML --yes
 rem python setup.py bdist_wheel
 
-set TIMEZONE_PATCH=0
+set TIMEZONE_PATCH=1
 
 set PATH=C:\Miniconda36;C:\Miniconda36\Scripts;%PATH%
+conda install Cython PyYAML --yes
+python setup.py bdist_wheel
+
+set TIMEZONE_PATCH=1
+
+set PATH=C:\Miniconda37;C:\Miniconda37\Scripts;%PATH%
 conda install Cython PyYAML --yes
 python setup.py bdist_wheel
--- res/build_64.bat
+++ res/build_64.bat
@@ -7,12 +7,18 @@ rem python setup.py bdist_wheel
 
 rem set TIMEZONE_PATCH=0
 
-set PATH=C:\Miniconda35-x64;C:\Miniconda35-x64\Scripts;%PATH%
+rem set PATH=C:\Miniconda35-x64;C:\Miniconda35-x64\Scripts;%PATH%
+rem conda install Cython PyYAML --yes
+rem python setup.py bdist_wheel
+
+set TIMEZONE_PATCH=1
+
+PATH=C:\Miniconda36-x64;C:\Miniconda36-x64\Scripts;%PATH%
 conda install Cython PyYAML --yes
 python setup.py bdist_wheel
 
-set TIMEZONE_PATCH=0
+set TIMEZONE_PATCH=1
 
-PATH=C:\Miniconda36-x64;C:\Miniconda36-x64\Scripts;%PATH%
+PATH=C:\Miniconda37-x64;C:\Miniconda37-x64\Scripts;%PATH%
 conda install Cython PyYAML --yes
 python setup.py bdist_wheel
