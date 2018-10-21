a10e7a48b75b7c699324a0d205ebc8f731a67236
simonflueckiger <simon@flueckiger.ch>
2018-10-21 15:31:50 -0700

    minor fix

--- conda/create_meta.bat
+++ conda/create_meta.bat
@@ -1,18 +1,15 @@
-(
-echo package:
-echo     name: tesserocr
-echo     version: %$TESSEROCR_VER%
-echo build:
-echo   number: %$BUILD_NUMBER%
-echo   string: tesseract_%$TESSERACT_VER%_%$BUILD_NUMBER%
-echo requirements:
-echo   build:
-echo     - python  
-echo   run:
-echo     - python
-echo about:
-echo     home: https://github.com/sirfz/tesserocr
-echo     license: MIT
-echo     summary: A simple, Pillow-friendly, Python wrapper around tesseract-ocr API using
-echo         Cython
-) > meta.yaml
+echo package: > meta.yaml
+echo     name: tesserocr >> meta.yaml
+echo     version: %$TESSEROCR_VER% >> meta.yaml
+echo build: >> meta.yaml
+echo   number: %$BUILD_NUMBER% >> meta.yaml
+echo   string: tesseract_%$TESSERACT_VER%_%$BUILD_NUMBER% >> meta.yaml
+echo requirements: >> meta.yaml
+echo   build: >> meta.yaml
+echo     - python >> meta.yaml
+echo   run: >> meta.yaml
+echo     - python >> meta.yaml
+echo about: >> meta.yaml
+echo     home: https://github.com/sirfz/tesserocr >> meta.yaml
+echo     license: MIT >> meta.yaml
+echo     summary: A simple, Pillow-friendly, Python wrapper around tesseract-ocr API using Cython >> meta.yaml
